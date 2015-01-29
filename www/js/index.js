0a42aa3898b306af881c6233f9645dbabf404c7f
Andy Lee <alee@scottlogic.co.uk>
2015-01-29 16:49:04 +0000

    Simplified startup

--- www/js/index.js
+++ www/js/index.js
@@ -11,41 +11,13 @@
         shim: { 'angularAMD': ['angular'], 'angular-route': ['angular'] }
     });
 
-    require(["require"], function (require) {
-        var app = {
-            // Application Constructor
-            initialize: function () {
-                this.bindEvents();
-            },
-            // Bind Event Listeners
-            //
-            // Bind any events that are required on startup. Common events are:
-            // 'load', 'deviceready', 'offline', and 'online'.
-            bindEvents: function () {
-                document.addEventListener('deviceready', this.onDeviceReady, false);
-            },
-            // deviceready Event Handler
-            //
-            // The scope of 'this' is the event. In order to call the 'receivedEvent'
-            // function, we must explicitly call 'app.receivedEvent(...);'
-            onDeviceReady: function () {
-                app.receivedEvent('deviceready');
-                start();
-            },
-            // Update DOM on a Received Event
-            receivedEvent: function (id) {
-                console.log('Received Event: ' + id);
-            }
-        };
+    var start = function start() {
+        require(["app"]);
+    };
 
-        var start = function start() {
-            require(["app"]);
-        };
-
-        if (window.cordova) {
-            app.initialize();
-        } else {
-            start();
-        }
-    });
+    if (window.cordova) {
+        document.addEventListener('deviceready', start, false);
+    } else {
+        start();
+    }
 })();
